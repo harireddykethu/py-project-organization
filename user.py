5de7145d172aadc39817c1d6c71bc6cd596f2a78
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-18 19:38:45 +0530

    done

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 # https://docs.python.org/3/library/functions.html#dir
 
 import lib.general.source as source_module
+import builtins
 
 print('Names in the source module:')
 
@@ -16,3 +17,9 @@ for name in dir():
     # source_module is also a name in the current module
     # also, 'name' variable introduced by this iterator block
     # is also a name
+
+print('---------------------------------------')
+
+print('Names in the builtins module (all errors and global functions):')
+for name in dir(builtins):
+    print(name)
