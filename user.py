da064179341f6689e9acac9542a9201470ffb9f7
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-18 19:02:05 +0530

    done

--- user.py
+++ user.py
@@ -2,6 +2,14 @@ from constants import PI
 from constants import cities as metro_cities    # aliasing
 from factorial import factorial
 
+# We execute the user.py module
+# That means, the __name__ of this module will be __main__
+
+if __name__ == '__main__':
+    print('Executing user.py')
+
+# However, this above block would not run if the user.py is imported
+
 print(f'π = {PI}')    # accessing variable declared in the module
 
 factorial_7 = factorial(7)
